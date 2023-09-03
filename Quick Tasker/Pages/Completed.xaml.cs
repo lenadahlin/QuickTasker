0c975383b4132b95b3d2487567b44dbcf57f6bd5
lenadahlin <lenakarind@hotmail.com>
2023-09-04 09:25:21 +1000

    started making completed page, all tasks are being marked as completed when only 1 should?

--- Quick Tasker/Pages/Completed.xaml.cs
+++ Quick Tasker/Pages/Completed.xaml.cs
@@ -1,9 +1,32 @@
-﻿namespace Quick_Tasker.Pages;
+﻿using AndroidX.Lifecycle;
+using Quick_Tasker.ViewModels;
+using Quick_Tasker.Models;
+
+namespace Quick_Tasker.Pages;
 
 public partial class Completed : ContentPage
 {
-	public Completed()
-	{
-		InitializeComponent();
-	}
+    private TaskViewModel viewModel;
+    public Completed()
+    {
+        BindingContext = viewModel = new TaskViewModel();
+        InitializeComponent();
+    }
+    private void CheckedComplete(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is CheckBox checkBox && checkBox.BindingContext is Tasks task)
+        {
+            if (e.Value)
+            {
+                // if checkbox is checked, change CompletedDate to today
+                task.CompletedDate = DateTime.Now;
+            }
+            else
+            {
+                // if checkbox is un-checked, change CompletedDate to null
+                task.CompletedDate = null;
+            }
+            viewModel.SaveTask(task);
+        }
+    }
 }
--- Quick Tasker/Pages/TaskBank.xaml.cs
+++ Quick Tasker/Pages/TaskBank.xaml.cs
@@ -1,14 +1,34 @@
 ﻿using AndroidX.Lifecycle;
 using Quick_Tasker.ViewModels;
 using Quick_Tasker.Models;
+using System.Threading.Tasks;
+
 namespace Quick_Tasker.Pages;
 
 public partial class TaskBank : ContentPage
 {
     private TaskViewModel viewModel;
     public TaskBank()
-	{
+    {
         BindingContext = viewModel = new TaskViewModel();
         InitializeComponent();
-	}
+    }
+    //TODO checking as complete is updating the date for EVERY task 
+    private void CheckedComplete(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is CheckBox checkBox && checkBox.BindingContext is Tasks task)
+        {
+            if (e.Value)
+            {
+                // if checkbox is checked, change CompletedDate to today
+                task.CompletedDate = DateTime.Now;
+            }
+            else
+            {
+                // if checkbox is un-checked, change CompletedDate to null
+                task.CompletedDate = null;
+            }
+            viewModel.SaveTask(task);
+        }
+    }
 }
